9e13003da7766259371a2fbb8b7138687a6cb3e2
Sagar Chandra Das <sagarthecoder@gmail.com>
2024-10-28 15:13:47 +0600

    Product price and title added in UI

--- lib/modules/ui-sections/dashboard/product/views/product-list/product-cell.dart
+++ lib/modules/ui-sections/dashboard/product/views/product-list/product-cell.dart
@@ -1,22 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce/modules/ui-sections/common-views/NetworkImageView.dart';
-import 'package:flutter_ecommerce/modules/ui-sections/dashboard/product/controller/product-controller.dart';
-import 'package:flutter_ecommerce/modules/ui-sections/dashboard/product/models/product-info.dart';
-import 'package:get/get.dart';
+import '../../models/product-info.dart';
 
 class ProductCell extends StatelessWidget {
   final ProductInfo productInfo;
 
-  ProductCell({required this.productInfo, super.key}) {}
+  ProductCell({required this.productInfo, super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Stack(
-        children: [
-          _buildImage(),
-          _buildRating(),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            spreadRadius: 2,
+            blurRadius: 6,
+            offset: const Offset(0, 3),
+          ),
         ],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(15),
+        child: Stack(
+          children: [
+            _buildImage(),
+            _buildRating(),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: _buildDescription(),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -30,32 +47,61 @@ class ProductCell extends StatelessWidget {
   }
 
   Widget _buildRating() {
-    return Padding(
-      padding: const EdgeInsets.only(top: 16, left: 16),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            color: Colors.white,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Icon(Icons.star_border_purple500_sharp),
-                const SizedBox(
-                  width: 10,
-                ),
-                Text(
-                  (productInfo.rating?.rate ?? 0.0).toStringAsFixed(1),
-                  style: const TextStyle(
-                      color: Colors.black,
-                      fontSize: 9,
-                      fontWeight: FontWeight.bold),
-                )
-              ],
+    return Positioned(
+      top: 10,
+      left: 10,
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 6, vertical: 4),
+        decoration: BoxDecoration(
+          color: Colors.yellow[700],
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Row(
+          children: [
+            Icon(Icons.star, color: Colors.white, size: 16),
+            SizedBox(width: 4),
+            Text(
+              (productInfo.rating?.rate ?? 0.0).toStringAsFixed(1),
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDescription() {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+      color: Colors.black.withOpacity(0.7),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            productInfo.title ?? "",
+            textAlign: TextAlign.center,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: const TextStyle(
+              fontSize: 16,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 4),
+          Text(
+            "\$${productInfo.price ?? 0.0}",
+            style: const TextStyle(
+              fontSize: 14,
+              color: Colors.pinkAccent,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
         ],
       ),
     );
--- lib/modules/ui-sections/dashboard/product/views/product-list/product-list-view.dart
+++ lib/modules/ui-sections/dashboard/product/views/product-list/product-list-view.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce/modules/ui-sections/dashboard/product/views/product-list/product-cell.dart';
-import 'package:get/get.dart';
-
 import '../../models/product-info.dart';
 
 class ProductListView extends StatelessWidget {
@@ -11,18 +9,16 @@ class ProductListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
+      padding: const EdgeInsets.all(10),
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
-        mainAxisSpacing: 10,
-        crossAxisSpacing: 10,
+        mainAxisSpacing: 15,
+        crossAxisSpacing: 15,
+        childAspectRatio: 0.75,
       ),
       itemCount: products.length,
       itemBuilder: (context, index) {
-        return SizedBox(
-          width: 120,
-          height: 170,
-          child: ProductCell(productInfo: products[index]),
-        );
+        return ProductCell(productInfo: products[index]);
       },
     );
   }
